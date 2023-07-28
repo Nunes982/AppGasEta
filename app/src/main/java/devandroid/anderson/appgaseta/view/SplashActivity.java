7b9581ea8722cd4a1633ef20187f0657a488a359
Nunes1982 <ansso06061982@gmail.com>
2023-07-28 11:47:29 -0300

    Criado Classe GAsEtaDB, e duas tabelas e instanciando na tela SplashActivity.

--- app/src/main/java/devandroid/anderson/appgaseta/view/SplashActivity.java
+++ app/src/main/java/devandroid/anderson/appgaseta/view/SplashActivity.java
@@ -1,14 +1,17 @@
 package devandroid.anderson.appgaseta.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import devandroid.anderson.appgaseta.R;
+import devandroid.anderson.appgaseta.database.GasEtaDB;
 
 public class SplashActivity extends AppCompatActivity {
+
+    GasEtaDB db;
 
     public static final int TIME_OUT_SPLASH = 3000;
 
@@ -17,21 +20,29 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        comutarTelaSplash();
+        cumutarTelaSplash();
+
 
     }
 
-    private void comutarTelaSplash() {
+    private void cumutarTelaSplash() {
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
 
-                Intent telaPrincipal = new Intent(SplashActivity.this, MainActivity.class);
+                db = new GasEtaDB(SplashActivity.this);
+
+                Intent telaPrincipal = new Intent(
+                        SplashActivity.this,
+                        GasEtaActivity.class);
+
                 startActivity(telaPrincipal);
                 finish();
 
             }
-        },TIME_OUT_SPLASH);
+        }, TIME_OUT_SPLASH);
+
+
     }
 }
